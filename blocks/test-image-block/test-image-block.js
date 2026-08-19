ee5e14500f3426bee29857bff0892d84bf057e58
Kush Rathod <kush.rathod@deptagency.com>
2026-08-19 14:19:19 +0530

    add imag

--- blocks/test-image-block/test-image-block.js
+++ blocks/test-image-block/test-image-block.js
@@ -1,3 +1,14 @@
+/**
+ * Decorates the authored background and overlay images as a title banner.
+ * @param {Element} block The test image block element
+ */
 export default function decorate(block) {
-  console.log("blokc", block);
+  const backgroundImage = block.querySelector('img[data-aue-prop="desktop-image"]');
+  const overlayImage = block.querySelector('img[data-aue-prop="desktop-overlay-image"]');
+
+  if (!backgroundImage || !overlayImage) return;
+
+  block.classList.add('test-image-banner');
+  backgroundImage.classList.add('test-image-banner-background');
+  overlayImage.classList.add('test-image-banner-overlay');
 }
