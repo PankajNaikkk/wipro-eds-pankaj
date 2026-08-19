ec542f813d754958e88e8920522799265202c918
Kush Rathod <kush.rathod@deptagency.com>
2026-08-19 17:58:45 +0530

    css change

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,17 +2,23 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
-  // setup image columns
   [...block.children].forEach((row) => {
+    row.classList.add('columns-row');
+
     [...row.children].forEach((col) => {
+      col.classList.add('columns-column');
+
       const pic = col.querySelector('picture');
       if (pic) {
-        const picWrapper = pic.closest('div');
-        if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
-          picWrapper.classList.add('columns-img-col');
-        }
+        const picWrapper = pic.parentElement;
+        picWrapper.classList.add('columns-image-wrapper');
+        pic.classList.add('columns-picture');
+        pic.querySelector('img').classList.add('columns-image');
+        col.classList.add('columns-img-col');
       }
+
+      const text = col.querySelector('[data-aue-prop="text"]');
+      if (text) text.classList.add('columns-text');
     });
   });
 }
