ca4388f15d56b16fcb1e20933fcc429daf8b7bdd
Kush Rathod <kush.rathod@deptagency.com>
2026-08-19 14:51:01 +0530

    add imag

--- blocks/test-image-block/test-image-block.js
+++ blocks/test-image-block/test-image-block.js
@@ -9,6 +9,9 @@ export default function decorate(block) {
   if (!backgroundImage || !overlayImage) return;
 
   block.classList.add('test-image-banner');
+  const [backgroundRow, overlayRow] = block.children;
+  backgroundRow.classList.add('test-image-banner-row', 'test-image-banner-background-row');
+  overlayRow.classList.add('test-image-banner-row', 'test-image-banner-overlay-row');
   backgroundImage.classList.add('test-image-banner-background');
   overlayImage.classList.add('test-image-banner-overlay');
 }
